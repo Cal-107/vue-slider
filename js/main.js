1db80f6d1ad87ef84099404f9a6f62f5290c8bd5
“Cal-107” <kaly___7@hotmail.it>
2021-11-12 13:38:19 +0100

    add setup

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Descrizione:
+// Partendo dalla struttura base, allegata sotto, rifare l’esercizio dello slider come fatto insieme in classe.
+// In allegato con la base con HTML e CSS trovate anche un file con i dati necessari per lo slider.
+// Bonus
+// Applicare l’autoplay allo slider: ogni 3 secondi cambia immagine automaticamente.
+
+/*
+ * Vue Slider
+ */
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum.',
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    image: 'img/05.jpg',
+                    title:
+                        'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                },
+            ]
+        }
+    }
+);
